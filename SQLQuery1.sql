a4ab5f8f0e1ad621616aa26dc911d6daf66b5d67
beforeneil <pveramoothea@gmail.com>
2024-09-02 13:44:50 +0200

    Insert sample data into DB

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO [dbo].[Product] ([Name], [Description], [Price])
+VALUES 
+(N'Basic Ladies Tee', N'100% cotton t-shirt in solid black.', 210),
+(N'Silver handcrafted necklace', N'Cable chain style unisex necklace in silver plated stainless steel.', 990),
+(N'Gold handcrafted necklace', N'Cable chain style unisex necklace in gold plated stainless steel.', 1750),
+(N'Unisex hoodie', N'100% cotton hoodie in solid black.', 280);
+
+
+SELECT * FROM [dbo].[Product];
