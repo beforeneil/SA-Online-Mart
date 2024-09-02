1190422c91e34973f13dfad8a032e86f037cb71a
beforeneil <pveramoothea@gmail.com>
2024-09-02 21:55:44 +0200

    Change some UI and add cart functionality

---
+++ Models/CartItem.cs
@@ -0,0 +1,15 @@
+﻿namespace EHA_Assignment_SAOnlineMart.Models
+{
+    public class CartItem
+    {
+        public int prodId { get; set; }
+        public string prodName { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public CartItem()
+        {
+            
+        }
+    }
+}
